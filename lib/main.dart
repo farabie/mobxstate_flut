b0fba41a7690254b25b8faac16c1198f5c4d3df0
Muhammad Farabie <farabie024@gmail.com>
2023-10-04 08:20:27 +0700

    mobxstate implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mobxstate_flut/mobxstate/counter.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+final CounterMobx counter = CounterMobx();
 
 class MyApp extends StatelessWidget {
   @override
@@ -19,38 +23,49 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "0",
-            style: TextStyle(fontSize: 80, fontWeight: FontWeight.w700),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Row(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mobx State Demo'),
+      ),
+      body: Center(
+        child: Observer(
+          builder: (context) => Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FloatingActionButton(
-                heroTag: 'Decrement',
-                onPressed: () {},
-                child: Icon(Icons.arrow_downward),
+              Text(
+                counter.value.toString(),
+                style: TextStyle(fontSize: 80, fontWeight: FontWeight.w700),
               ),
               SizedBox(
-                width: 20,
+                height: 40,
               ),
-              FloatingActionButton(
-                heroTag: 'Increment',
-                onPressed: () {},
-                child: Icon(
-                  Icons.arrow_upward,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    heroTag: 'Decrement',
+                    onPressed: () {
+                      counter.decrement();
+                    },
+                    child: Icon(Icons.arrow_downward),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  FloatingActionButton(
+                    heroTag: 'Increment',
+                    onPressed: () {
+                      counter.increment();
+                    },
+                    child: Icon(
+                      Icons.arrow_upward,
+                    ),
+                  )
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
